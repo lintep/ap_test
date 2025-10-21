3a1ab8a305297ef9fd22c9a79b5f1d51c5fec613
rahmani_saeed <s.rahmani.ce@gmail.com>
2025-10-21 12:13:27 +0330

    EX0_E0_2: Define Sudoku functions

---
+++ Sudoku.java
@@ -0,0 +1,19 @@
+public class Sudoku {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[][] loadSudokuFromFile(String fileName){
+        return null;
+    }
+
+    public static boolean checkSolvedSudoku(int[][] sudoku){
+        return false;
+    }
+
+
+    public static void saveSudokuToFile(String fileName){
+        return;
+    }
+
+}
